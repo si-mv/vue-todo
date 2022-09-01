ad9c3a7418046653036b07dc7f7da4402f69313d
si-mv <simon.haines@multiverse.io>
2022-09-01 15:32:57 +0100

    remove secret

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-GITHUB_SECRET = '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'
-
 const pinia = createPinia()
 
 createApp(App)
