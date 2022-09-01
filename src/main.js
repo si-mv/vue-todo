d4089f747252c3b57bd2b439f9511668ab8bba1b
si-mv <simon.haines@multiverse.io>
2022-09-01 15:32:36 +0100

    remove jwt

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibW9ycGhldXMiLCJpYXQiOjE2NTMxMDI3MzR9.zNYcb1c9v8EO-oszD8cs4LDEzuS-epfQINWHfwQ48SI'
-
 const pinia = createPinia()
 
 createApp(App)
